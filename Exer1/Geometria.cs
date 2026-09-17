50c6774acaee77c59bd78bcbf957717f58112230
thayssaalexandre <thayssa.alexandre@edu.senai.br>
2026-09-17 08:59:53 -0300

    Adicionar arquivos de projeto.

---
+++ Exer1/Geometria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Polimorfismo.Exer1
+{
+    internal class Geometria
+    {
+        public double CalcularArea(double Lado)
+        {
+            return Lado * Lado;
+        }
+        public double CalcularArea(double Altura, double Base)
+        {
+            return Altura * Base;
+        }
+        public double CalcularArea(double Raio, bool IsCirculo)
+        {
+                return Math.PI * Raio * Raio;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Polimorfismo.Exer1;
+using System.Net.Quic;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Geometria g = new Geometria();
+
+            double AreaQuadrado = g.CalcularArea(5);
+            double AreaRetangulo = g.CalcularArea(8, 20);
+            double AreaCirculo = g.CalcularArea(5, true);
+
+            Console.WriteLine("Área do Quadrado: " + AreaQuadrado);
+            Console.WriteLine("Área do Retângulo: " + AreaRetangulo);
+            Console.WriteLine("Área do Círculo: " + AreaCirculo);
+
+
+        }
+    }
+}
